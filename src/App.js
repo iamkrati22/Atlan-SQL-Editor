42e007a2371b2a082b4abcbf6f998b14192597a9
iamkrati22 <iamkrati22@gmail.com>
2023-07-19 22:23:09 +0530

    :zap: Optimization done to improve performance-4

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Footer from './components/Common/Footer';
 import Navbar from './components/Common/Navbar';
 import Editor from './components/Main/Editor';
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Instructions from './components/Main/Instruction';
 
